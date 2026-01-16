4290b12dacebd991e70683065f48af3b8884f35c
nlp-MItKs <1773522383@qq.com>
2026-01-16 16:53:01 +0800

    Update 作业2.py

--- 尹鑫/week01/作业2.py
+++ 尹鑫/week01/作业2.py
@@ -24,7 +24,7 @@ model.fit(input_feature, dataset[1].values)
 client = OpenAI(
     # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx",
     # https://bailian.console.aliyun.com/?tab=model#/api-key
-    api_key="sk-e203625e9ae544c8a8ac37a7b3d090ae", # 账号绑定，用来计费的
+    api_key="sk-e203625xxxxx7a7b3d090ae", # 账号绑定，用来计费的
 
     # 大模型厂商的地址，阿里云
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
